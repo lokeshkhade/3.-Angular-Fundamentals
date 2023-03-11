56aaca64d535efbabb208bb9cdd513720e361f65
lokeshkhade@gmail.com <lokeshkhade@gmail.com>
2023-03-11 23:14:35 +0530

    Component

---
+++ student/src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'courses',
+    template: '<h2>Courses</h2>',
+    styles: ['h2 { font-weight: normal; }']
+})
+
+export class CoursesComponent {
+
+}
